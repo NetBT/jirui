2a4e7582da313e7d5ec087082658142c58a493a0
fuzhuangzhuang <810513844@qq.com>
2018-10-16 20:24:51 +0800

    数据库model

--- backend/models/MemberOrderImage.php
+++ backend/models/MemberOrderImage.php
@@ -2,7 +2,6 @@
 
 namespace backend\models;
 
-use common\models\Status;
 use Yii;
 use yii\base\Exception;
 use common\models\Functions;
@@ -24,6 +23,63 @@ class MemberOrderImage extends Common
     public static function tableName()
     {
         return '{{%ab_member_order_images}}';
+    }
+
+    public function rules()
+    {
+        return [
+            [
+                [
+                    'member_order_id',
+                    'path',
+                    'type',
+                    'filename',
+                    'size',
+                    'created_at',
+                    'updated_at',
+                ],
+                'required',
+            ],
+            [
+                [
+                    'path',
+                    'type',
+                    'filename',
+                ],
+                'string',
+            ],
+
+            ['size', 'integer'],
+            ['member_order_id', 'integer'],
+            ['member_order_id', 'integer'],
+            [['uploadedImage'], 'file', 'skipOnEmpty' => false, 'extensions' => 'jpg,png,svg,jpeg,gif'],
+        ];
+    }
+
+    public function uploadImage()
+    {
+        try {
+            $res = Functions::uploadFile('file');
+            $id = Yii::$app->request->get('order_id');
+            if ($res === false) {
+                throw new \Exception(false);
+            }
+            if (!empty($id)) {
+                $data['member_order_id'] = $id;
+                $path_info = pathinfo($res);
+                $data['path'] = $path_info['dirname'];
+                $data['filename'] = $path_info['basename'];
+                $data['type'] = $path_info['extension'];
+                if (!($this->load($data) && $this->save())) {
+                    throw new \Exception(array_shift($this->firstErrors));
+                }
+            } else {
+                throw new \Exception(false);
+            }
+            return Functions::formatJson(1000, '上传成功', $res);
+        } catch (Exception $e) {
+            return Functions::formatJson(2000, $e->getMessage());
+        }
     }
 
 }
